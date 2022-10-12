9f170f94b455de843712c2efdda485c3207de17b
Manasi <manasitilak12@gmail.com>
2022-10-12 08:59:08 +0530

    integrated django react

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import PrivateRoute from './utils/PrivateRoute';
+import { AuthProvider } from './context/AuthContext'
+
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <AuthProvider>
+        <Header />
+        <Routes>
+          
+          <Route  
+            exact path="/" 
+            element= {
+              <PrivateRoute>
+                <HomePage />
+              </PrivateRoute>
+             }
+            />
+          <Route element= {<LoginPage />} path="/login" />
+        </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ backend/frontend/src/pages/HomePage.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect, useContext} from 'react'
+import AuthContext from '../context/AuthContext';
+import useAxios from '../utils/useAxios';
+
+
+const HomePage = () => {
+  let[notes, setNotes] = useState([])
+  let{authTokens, logoutUser} = useContext(AuthContext)
+
+  let api = useAxios()
+
+  useEffect(()=>{
+    getNotes()
+  },[])
+
+  let getNotes = async()=>{
+    let response = await api.get('api/notes')
+
+    if(response.status === 200){
+      setNotes(response.data)
+    }
+    
+  }
+  return (
+    <div>
+      <p>You are Logged in to the Home Page</p>
+
+      <ul>
+        {notes.map(note =>(
+          <li key={note.id}>{note.body}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default HomePage
